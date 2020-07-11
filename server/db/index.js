ffd3537b1cf2ad62ffb7a841cd0f1e41be7e730e
Vineeth Suvarna <vineeth@forkmedia.in>
2020-07-11 11:43:53 +0530

    Adds server files

---
+++ server/db/index.js
@@ -0,0 +1,6 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const path = require('path')
+
+const adapter = new FileSync(path.join(__dirname, '../data/imdb.json'))
+const db = low(adapter)
